fa22e1f83eb21b783fba3a976b9525c79cfe9a79
Soo-Bin <weeiqp@gmail.com>
2017-04-04 14:25:18 +0900

    Updated main.cpp

--- image_pro/main.cpp
+++ image_pro/main.cpp
@@ -5,9 +5,16 @@
 
 void main_1_week();
 void main_2_week_histogram();
+void main_3_week_AvgFiltering();
+void main_4_week_tmmplate_matching();
+void main_5_week_color_image();
 
-void main() 
+void main()
 {
 	//main_1_week();
-	main_2_week_histogram();
+	//main_2_week_histogram();
+	//main_3_week_AvgFiltering();
+	//main_4_week_tmmplate_matching();
+	main_5_week_color_image();
+	
 }
