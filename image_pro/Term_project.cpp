144788687f6613e5ab4d29a9686d7c47f6bda003
Soo-Bin <weeiqp@gmail.com>
2017-06-13 01:41:05 +0900

    Updated Term_project.cpp

--- image_pro/Term_project.cpp
+++ image_pro/Term_project.cpp
@@ -28,9 +28,8 @@ void IntFree2(int** image, int width, int height);
 int** ReadImage(char* name, int* width, int* height);
 void WriteImage(char* name, int** image, int width, int height);
 void ImageShow(char* winname, int** image, int width, int height);
-void CopyImage(int** src, int width, int height, int** dst);
 
-int** CutImage(char* name, int* width, int* height)
+int** CutImage(char* name, int* width, int* height)	//ReadImage를 참조
 {
 	Mat img = imread(name, IMREAD_GRAYSCALE);
 	int minY = img.rows, minX = img.cols;
@@ -65,7 +64,7 @@ ERR PrintNumber(int sum_i[])
 	for (int i = 0; i < 10; i++)
 		if (sum_i[i] == error.minError)
 			error.index = i;
-	
+
 	return  (error);
 }
 
@@ -103,7 +102,7 @@ int B_Interpolation(int** image_out, int height, int width, double y, double x)
 	}
 
 	result = P1*(1 - delta_x)*(1 - delta_y) + P2*(delta_x)*(1 - delta_y) + P3*(1 - delta_x)*(delta_y)+P4*(delta_x)*(delta_y);
-	
+
 	return (int)result;
 }
 
@@ -116,14 +115,14 @@ int** ResizeImage(int** image, int width, int height, int bw, int bh)
 	for (int y = 0; y < bh; y++)
 		for (int x = 0; x < bw; x++)
 			image_out[y][x] = B_Interpolation(image, height, width, (double)y / ratio_y, (double)x / ratio_x);
-	
+
 	return (image_out);
 }
 
 int** Diagonalmage(int** image, int width, int height, int bw, int bh)
 {
 	int** image_rotate = (int**)IntAlloc2(width * 2, height * 2);
-	
+
 	dot_point P1, P2;
 
 	P1.x = width;	P1.y = 0;
@@ -145,14 +144,14 @@ int** Diagonalmage(int** image, int width, int height, int bw, int bh)
 			double _y = (ycenter + ((double)y - ycenter)*cc - ((double)x - xcenter)*ss);
 			image_rotate[y][x] = B_Interpolation(image, height, width, _y, _x);
 		}
-		
+
 	return (image_rotate);
 }
 
 void main_Term_project()
 {
 	int width, height;
-	int** img = CutImage(".\\number\\0_resize.bmp", &width, &height);
+	int** img = CutImage(".\\number_big\\9_resize_big.bmp", &width, &height);
 	int** image_out, **image_rotate;
 	ERR error, error1;
 
@@ -160,7 +159,7 @@ void main_Term_project()
 	int** db[NUM];
 	int sum_i[10];
 	int bw, bh;
-	
+
 	for (int i = 0; i < NUM; i++) {
 		sprintf(name, ".\\number\\%01d_org.bmp", i);
 		db[i] = (int**)CutImage(name, &bw, &bh);
@@ -168,19 +167,17 @@ void main_Term_project()
 		TempleteMatching(image_out, db[i], bw, bh, i, sum_i);
 		IntFree2(db[i], bw, bh);
 	}
-	
+
 	error = PrintNumber(sum_i);
-	
-	printf("인식된 숫자는 %d입니다! minerror = %d \n", error.index, error.minError);
-	
+
 	IntFree2(image_out, bw, bh);
 
-	image_rotate = Diagonalmage(img, width, height, bw, bh);
+	image_rotate = Diagonalmage(img, width, height, bw, bh);	//대각선 긋기
 	WriteImage("image_rotate.bmp", image_rotate, width * 2, height * 2);
 	IntFree2(image_rotate, width * 2, height * 2);
 
 	image_out = CutImage("image_rotate.bmp", &width, &height);
-	
+
 	for (int i = 0; i < NUM; i++) {
 		sprintf(name, ".\\number\\%01d_org.bmp", i);
 		db[i] = (int**)CutImage(name, &bw, &bh);
@@ -188,14 +185,17 @@ void main_Term_project()
 		TempleteMatching(image_rotate, db[i], bw, bh, i, sum_i);
 		IntFree2(db[i], bw, bh);
 	}
-	
+
 	error1 = PrintNumber(sum_i);
 
-	printf("인식된 숫자는 %d입니다! minerror = %d \n", error1.index, error1.minError);
+	if (error.minError < error1.minError)
+		printf("인식된 숫자는 %d입니다!\n", error.index, error.minError);
+	else
+		printf("인식된 숫자는 %d입니다!\n", error1.index, error1.minError);
 
-	//ImageShow("Number Image", img, bw, bh);
-	//waitKey(0);
+	ImageShow("Number Image", img, bw, bh);
+	waitKey(0);
 
-	//IntFree2(img, width, height);
-	
+	IntFree2(img, width, height);
+
 }
